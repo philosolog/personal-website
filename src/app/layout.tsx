9af7275c7c3584e102b0bcdc854881137addab3d
philosolog <philosolog@gmail.com>
2024-09-05 04:22:00 -0700

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,23 @@ const hanken_grotesk = Hanken_Grotesk({
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Aaron Wang",
-	description: "An undergraduate with a fervor for all things in the intersection of pure mathematics and computer science.",
+	// title: "Aaron Wang",
+	// description: "An undergraduate with a fervor for all things pure mathematics and computer science.",
+	openGraph: {
+		title: 'Aaron Wang',
+		description: 'An undergraduate with a fervor for all things pure mathematics and computer science.',
+		url: 'https://philosolog.com',
+		siteName: 'philosolog',
+		images: [
+			{
+				url: 'https://philosolog.com/philosolog_logo_dark.jpg', // Must be an absolute URL
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: 'en_US',
+		type: 'website',
+	},
 };
 
 export default function RootLayout({
